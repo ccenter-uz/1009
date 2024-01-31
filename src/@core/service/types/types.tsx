82e611dfd9cc8e6531b0bd24bfe834fa46a5ea2e
Muhammadkhuja <muxammad_205@mail.ru>
2024-01-31 08:21:36 +0500

    proccess of creating reusable table

--- src/@core/service/types/types.tsx
+++ src/@core/service/types/types.tsx
@@ -6,11 +6,31 @@ export type Iuser = {
   confirm_password: string
 }
 
+// AUTH
 export type Ipin = {
   pin: string
 }
-
 export type Ilogin = {
   number: string
   password: string
+}
+
+// TABLE 
+export type IcolumnTable ={
+  title: string;
+  dataIndex: string;
+  key: string;
+  align?: 'left' | 'center' | 'right' | any;
+  width?:string,
+  height?:string
+}
+export type IrowTable ={
+  key: string;
+  [key: string]: React.ReactNode;
+}
+export type ItableType={
+  columns:IcolumnTable[]
+  dataSource:IrowTable[]
+  variant :'striped' | 'simple',
+  ColBg?:string
 }
---
+++ src/app/[locale]/opportunities/loading.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Loading: FC = () => {
+  return (
+    <section style={{ height: '100dvh' }}>
+      <h1>LOADING...</h1>
+    </section>
+  )
+}
+
+export default Loading
